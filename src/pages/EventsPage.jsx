b956a221522a09ab02151efec47671da8308a0fa
Tejasvi Verma <130896989+tejasviverma@users.noreply.github.com>
2024-08-15 22:07:52 +0530

    Update EventsPage.jsx

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,72 +1,48 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
+import React from 'react';
+import Slider from 'react-slick';
 import Navbar from '../components/Navbar';
-
-function CustomTabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-        </div>
-    );
-}
-
-CustomTabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
-}
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function EventsPage() {
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
     };
 
     return (
         <div className="bg-[#17153B]">
-            <Navbar></Navbar>
-            <Box sx={{ width: '100%', height: '90vh', p: 3 }}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                        <Tab sx={{
-                            color: "white", '&.Mui-selected': {
-                                color: '#1890ff'
-                            }
-                        }} label="Past" {...a11yProps(0)} />
-                        <Tab sx={{ color: "white" }} label="Present" {...a11yProps(1)} />
-                        <Tab sx={{ color: "white" }} label="Upcoming" {...a11yProps(2)} />
-                    </Tabs>
-                </Box>
-                <CustomTabPanel value={value} index={0}>
-                    New events
-                </CustomTabPanel>
-                <CustomTabPanel value={value} index={1}>
-                    Presentt
-                </CustomTabPanel>
-                <CustomTabPanel value={value} index={2}>
-                    All are here
-                </CustomTabPanel>
-            </Box>
+            <Navbar />
+            <div style={{ padding: '20px 30px', height: '90vh' }}>
+                <Slider {...settings}>
+                    <div>
+                        <img
+                            src="https://via.placeholder.com/800x400?text=New+Events"
+                            alt="New Events"
+                            style={{ width: '100%', height: '500px', borderRadius: '0px' }}
+                        />
+                    </div>
+                    <div>
+                        <img
+                            src="https://via.placeholder.com/800x400?text=Present+Events"
+                            alt="Present Events"
+                            style={{ width: '100%', height: '500px', borderRadius: '0px' }}
+                        />
+                    </div>
+                    <div>
+                        <img
+                            src="https://via.placeholder.com/800x400?text=Upcoming+Events"
+                            alt="Upcoming Events"
+                            style={{ width: '100%', height: '500px', borderRadius: '0px' }}
+                        />
+                    </div>
+                </Slider>
+            </div>
         </div>
-
     );
 }
+
